f238daa99d0755d79a4c73ef1c277aac1bbf1fd8
priya-002 <priya.md0002@gmail.com>
2021-07-05 20:36:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import YourForm from "./form/YourForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{marginLeft:"30%",marginRight:"70%",width:"100%", marginTop:"2%"}}>Building My First Form</h1>
+     <YourForm/>
+      
+      
     </div>
   );
 }
---
+++ src/form/YourForm.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Form,Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import"./form.css"
+import { useState } from 'react';
+
+const YourForm = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [submit, setSubmit] = useState('')
+
+    const first=(e)=>{
+        
+        setEmail(e.target.value)
+        console.log(email)
+}
+const second=(e)=>{
+        
+    setPassword(e.target.value)
+    console.log( "e",password)
+}
+const third=(e)=>{
+        
+e.preventDefault()
+  setSubmit(e.target.value)
+  const p={email,password}
+    console.log(p)
+}
+
+    return (
+        <div>
+           <div className="form">
+           
+           <Form style={{marginTop:"5%"}} onSubmit={third}>
+<Form.Group controlId="formBasicEmail">
+ <Form.Label>Email address</Form.Label>
+ <Form.Control type="text" placeholder="Enter email"  onChange={first}/> 
+ <Form.Text className="text-muted">
+   We'll never share your email with anyone else.
+ </Form.Text>
+</Form.Group>
+
+<Form.Group controlId="formBasicPassword">
+ <Form.Label>Password</Form.Label>
+ <Form.Control type="password" placeholder="Password" onChange={second} />
+</Form.Group>
+<Form.Group controlId="formBasicCheckbox">
+ <Form.Check type="checkbox" label="Check me out" />
+</Form.Group>
+<Button variant="primary" type="submit"  >
+ Submit
+</Button>
+</Form>
+     </div> 
+     <h1>My email {email}</h1>
+     <h1>password {password}</h1>
+        </div>
+    )
+}
+
+export default YourForm
